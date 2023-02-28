0aa1ac60807f4bdc34b4f163c48869573665dab8
Agyemang-SE <owusu.kevin@proton.me>
2023-02-28 16:13:22 +0000

    Data_Science files

---
+++ Database/create_drop.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Test (
+    ID integer,
+    Name VARCHAR(30)
+);
+
+create table COUNTRY (
+ID integer,
+CCODE char(2),
+NAME varchar(30)
+);
+
+create table COUNTRY (
+ID int not NULL,
+CCODE char(2),
+NAME varchar(60),
+primary key (ID)
+);
+
